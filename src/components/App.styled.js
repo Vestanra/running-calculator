605224751027101b4e374ea63f7ad5bf6ee7efe3
Vira <35749970+Vestanra@users.noreply.github.com>
2024-02-13 23:11:26 +0200

    update

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -32,7 +32,7 @@ export const WrapThougts = styled.div`
 font-size: 14px;
 color: rgba(35, 31, 32, 0.8);
 font-style: italic;
-padding: 0 28px;
+padding: 12px 28px;
 @media  (min-width: 768px){
     padding: 0 120px;
     font-size: 16px;
